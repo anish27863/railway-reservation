a297c1f7622dbdd8716e22673d39fb83f7bd63b2
Anish Saha <anish27863@gmail.com>
2024-02-02 15:55:40 +0530

    indentation errors fixed

--- reserv.py
+++ reserv.py
@@ -18,24 +18,24 @@ class train:
         self.seats = {'1AC' : seat_available_in_1AC, '2AC': seat_available_in_2AC,'SL': seat_available_in_SL}
         self.fare = {'1AC' : fare_1ac,'2AC' : fare_2ac ,'SL' : fare_sl}
     def print_seat_availablity(self):
-    	print("No. of seats available in 1AC :- "+str(self.seats['1AC']))
-    	print("No. of seats available in 2AC :- "+str(self.seats['2AC']))
-    	print("No. of seats available in SL :- "+str(self.seats['SL']))
+        print("No. of seats available in 1AC :- "+str(self.seats['1AC']))
+        print("No. of seats available in 2AC :- "+str(self.seats['2AC']))
+        print("No. of seats available in SL :- "+str(self.seats['SL']))
     def check_availabilty(self,coach='',ticket_num = 0):
-    	coach = coach.upper()
-    	if coach not in ('SL','1AC','2AC'):
-    		print_seat_availablity()
-    		coach = input("Enter the coach(1AC/2AC/SL) :-")
-    	else:
-    		if self.seats[coach] == 0:
-    			return False
-    		elif self.seats[coach] >= ticket_num:
-    			return True
-    		else:
-    			return False
+        coach = coach.upper()
+        if coach not in ('SL','1AC','2AC'):
+            print("seat_availablity")
+            coach = input("Enter the coach(1AC/2AC/SL) :-")
+        else:
+            if self.seats[coach] == 0:
+                return False
+            elif self.seats[coach] >= ticket_num:
+                return True
+            else:
+                return False
     def book_ticket(self,coach = '',no_of_tickets = 0):
-    	self.seats[coach] -= no_of_tickets
-    	return True
+        self.seats[coach] -= no_of_tickets
+        return True
 
 
 class ticket:
